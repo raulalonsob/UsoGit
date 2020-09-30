24f67621a2b15a0221b6c2fcf08aacbfb311b3a1
Raul Alonso <raul@raul>
2020-09-30 18:28:57 +0200

    Ejercicio 3

--- Ejercicios/javascript.js
+++ Ejercicios/javascript.js
@@ -1,1 +1,14 @@
 alert("¡Hola Mundo 2!");
+
+function mostrarVariables(){
+    alert("Variable1: " + variable1);
+    variable2 = null;
+    alert("Variable2: " + variable2);
+    var variable1 = 34;
+    alert("Variable1 declarada: " + variable1);
+    variable3 = 15.9;
+    alert("Variable1 * Variable3: " + (variable1 * variable3));
+    variable3 = "Ahora soy un texto";
+    alert("Variable3: " + variable3);
+    alert("Variable3 * 35: " + (variable3 * 35));
+}
